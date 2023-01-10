5a854de1f4f9494c72dc58aeb11cb6f0085bde59
Eduardo da Cunha <eduardodacunha9188@gmail.com>
2023-01-10 20:40:02 -0300

    Alterando script.sql

--- script.sql
+++ script.sql
@@ -13,3 +13,16 @@ create table contato(
 	e_mail varchar(100),
 	cpf integer 
 );
+INSERT INTO contato (nome, sobrenome, empresa, telefone, tipo_telefone, e_mail, cpf)
+VALUES ('alicio', 'da cunha', 'neogrid', 123, 'celular', 'alicio.fcn@gmail.com', 888);
+
+select * from contato;
+select nome, sobrenome, empresa, telefone, tipo_telefone, e_mail, cpf from contato;
+
+insert into contato (nome, sobrenome, empresa, telefone, tipo_telefone, e_mail, cpf)
+values 				('dudu', 'de quadros', 'fort', 123, 'residencia', 'dudu@gmail.com', 888); 
+
+select * from contato
+
+# excluir uma tabela
+drop table contato;
